6906d77979afaf26bb60d20f8ae06cb4de357dc0
ubilby <ubilby@gmail.com>
2023-01-28 12:12:59 +0400

    create base class

--- main.py
+++ main.py
@@ -2,6 +2,48 @@ from random import randint
 
 from graphic_arts.start_game_banner import run_screensaver
 
+DEFAULT_ATTACK = 5
+DEFAULT_DEFENCE = 10
+
+
+class Character:
+    BRIEF_DESC_CHAR_CLASS = 'отважный любитель приключений'
+    RANGE_VALUE_ATTACK = (1, 3)
+    RANGE_VALUE_DEFENCE = (1, 5)
+    SPECIAL_BUFF = 15
+    SPECIAL_SKILL = 'Удача'
+
+    def __init__(self, name: str):
+        self.name: str = name
+
+    def atack(self):
+        """Метод атаки базового класса."""
+        value_attack = DEFAULT_ATTACK + randint(*self.RANGE_VALUE_ATTACK)
+        return (f'{self.name} нанёс противнику урон, равный {value_attack}')
+
+    def defence(self):
+        value_defence = DEFAULT_DEFENCE + randint(*self.RANGE_VALUE_DEFENCE)
+        return (f'{self.name} блокировал {value_defence} ед. урона.')
+
+    def special(self):
+        return (f'{self.name} применил специальное умение '
+                f'"{self.SPECIAL_SKILL} {self.SPECIAL_BUFF}".')
+
+    def __str__(self):
+        return f'{self.__class__.__name__} - {self.BRIEF_DESC_CHAR_CLASS}.'
+
+
+class Warrior(Character):
+    pass
+
+
+class Mage(Character):
+    pass
+
+
+class Healer(Character):
+    pass
+
 
 def attack(char_name: str, char_class: str) -> str:
     """Atack function."""
